dd68e84ae8da84cfb626a74cd23ac52451d9d6b7
JIYACHINDADI <jiyachindadi@gmail.com>
2023-05-13 14:56:33 +0530

    one to many relationship in django by foreign key attribute in django model consect

--- gs102/myapp/admin.py
+++ gs102/myapp/admin.py
@@ -10,3 +10,10 @@ from myapp.models import Page
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
     list_display = ["user", "page_name", "page_desc"]
+
+from myapp.models import Post
+
+
+@admin.register(Post)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ["id",  "name", "description", "user"]
--- gs102/myapp/models.py
+++ gs102/myapp/models.py
@@ -10,3 +10,26 @@ class Page(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, limit_choices_to={'is_staff': True})
     page_name = models.CharField(max_length=40)
     page_desc = models.CharField(max_length=100)
+
+
+
+
+class Post(models.Model):
+    # ondelete = models.CASCAD 
+    # case when removing the user(parent) - post also get removed
+    # case when removing the post- only post will remove
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    # ondelete = models.PROTECT
+    # case when removing the user(parent) - it will not let to remove the user untill you remove all the post
+    #  case when removing the post- only post will remove
+    # user = models.ForeignKey(User, on_delete=models.PROTECT)
+    
+    
+    # ondelete = models.SET_NULL
+    # case when removing the user(parent) - post will not removed insted user set to null
+    # case when removing the post- only post will remove
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
