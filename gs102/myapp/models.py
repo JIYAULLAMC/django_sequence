d36c0244d269e138caac4abcf640345d877fda4c
JIYACHINDADI <jiyachindadi@gmail.com>
2023-05-13 09:53:34 +0530

    one-to-one-field protect help not to delete the user when any pages are associated with the user

--- gs102/myapp/models.py
+++ gs102/myapp/models.py
@@ -5,6 +5,9 @@ from django.contrib.auth. models import User
 
 
 class Page(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    # cascad used when the user removed the page respective to same user also get removed
+    # user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    # protect help not to delete the user when any pages are associated with the user
+    user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True)
     page_name = models.CharField(max_length=40)
     page_desc = models.CharField(max_length=100)
