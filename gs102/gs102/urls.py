47f6e0b9655605058de93482fd3b770893dd739c
JIYACHINDADI <jiyachindadi@gmail.com>
2023-05-14 09:48:59 +0530

    it is important that we can fetch the data present in another module, using user module withou any explicit relation

--- gs102/gs102/urls.py
+++ gs102/gs102/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from school import views
+from myapp import views as myappviews
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home)
+    path("", views.home),
+    path("my/", myappviews.myfunc)
 ]
--- gs102/myapp/views.py
+++ gs102/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from myapp.models import User, Post
+
+from django.http import HttpResponse
+
+def myfunc(request):
+    data = User.objects.filter(page__id=3)
+    print(data)
+    data = User.objects.filter(post__id=5)
+    print(data)
+    return HttpResponse(data)
