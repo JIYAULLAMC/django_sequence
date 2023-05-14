bd6426e3c7c7b1063ecbc15f8b053dd438c2bec3
JIYACHINDADI <jiyachindadi@gmail.com>
2023-05-14 09:49:42 +0530

    it is important that we can fetch the data present in another module, using user module withou any explicit relation

--- gs102/myapp/views.py
+++ gs102/myapp/views.py
@@ -7,6 +7,10 @@ from myapp.models import User, Post
 from django.http import HttpResponse
 
 def myfunc(request):
+    # this is very important because
+    # i have hot linked the page module to the user module 
+    # event though i am able to filter page data using user module
+    # make sure you have linked user module to the page user= models.Foreignkey(User)
     data = User.objects.filter(page__id=3)
     print(data)
     data = User.objects.filter(post__id=5)
