2cf86cc085a96fe6816e9e6594233f15b06a9862
JIYACHINDADI <jiyachindadi@gmail.com>
2023-06-07 23:46:37 +0530

    message framework working

--- gs76/enroll/views.py
+++ gs76/enroll/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.contrib import messages
 # Create your views here.
 
 from .forms import UserForm
@@ -11,6 +11,24 @@ def regi(request):
         fm = UserForm(request.POST)
         if fm.is_valid:
             fm.save()
+            print("-----------", messages.get_level(request))
+            messages.set_level(request, messages.SUCCESS)
+            messages.add_message(request, messages.SUCCESS, "your account has been created!")
+            print("-----------", messages.get_level(request))
+            messages.set_level(request, messages.INFO)
+            messages.add_message(request, messages.INFO, "your account has been created!")
+            print("-----------", messages.get_level(request))
+            messages.set_level(request, messages.WARNING)
+            messages.add_message(request, messages.WARNING, "your account has been created!")
+            print("-----------", messages.get_level(request))
+            messages.add_message(request, messages.ERROR, "your account has been created!")
+            messages.set_level(request, messages.ERROR)
+            print("-----------", messages.get_level(request))
+            messages.success(request, "your success account has been created!")
+            messages.info(request, "your info account has been created!")
+            messages.warning(request, "your  warning account has been created!")
+            messages.error(request, "your error account has been created!")
+
     else:
         fm =UserForm()
     return render(request, 'enroll/userregister.html', {'form' : fm})
