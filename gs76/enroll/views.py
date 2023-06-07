b1cd6f97c026459c6938805c45526f0ca9f57c1e
JIYACHINDADI <jiyachindadi@gmail.com>
2023-06-07 22:39:20 +0530

    created the model form admin and template

--- gs76/enroll/views.py
+++ gs76/enroll/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .forms import UserForm
+from .models import User
+
+def regi(request):
+
+    if request.method == 'POST':
+        fm = UserForm(request.POST)
+        if fm.is_valid:
+            fm.save()
+    else:
+        fm =UserForm()
+    return render(request, 'enroll/userregister.html', {'form' : fm})
